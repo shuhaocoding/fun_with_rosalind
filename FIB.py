63ffc015e81d1f6e4b0e28c7f6282eb56d34a84e
Shuhao Wang <137926005+shuhaocoding@users.noreply.github.com>
2024-01-30 01:57:55 -0500

    delete

--- FIB.py
+++
@@ -1,6 +0,0 @@
-n=36; k=4; a=[1,1]; i=2;
-while i<n:
-    x=a[-2]*k+a[-1]
-    a.append(x)
-    i=i+1
-print a[-1]
--- PMCH.py
+++
@@ -1,6 +0,0 @@
-import math
-from Bio import SeqIO
-for record in SeqIO.parse("rosalind_pmch.txt", "fasta"):
-    s = str(record.seq)
-# considering n pairs of A and U there will be n! perfect matchings of basepair edges
-print(math.factorial(s.count('A')) * math.factorial(s.count('C')))
